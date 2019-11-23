29e18db7c51571743a73d66dd980c41c6b99ab88
RobinHankin <hankin.robin@gmail.com>
2019-11-23 21:19:56 +1300

    switch to using boost dynamic bitsets

--- src/clifford.cpp
+++ src/clifford.cpp
@@ -9,18 +9,21 @@
 #include <iterator>
 #include <vector>
 #include <assert.h>
+#include <tuple>
+#include <boost/dynamic_bitset.hpp>
 
 using namespace std;
 using namespace Rcpp;
 
-typedef std::vector<bool> blade;  // technically wrong but...
+typedef boost::dynamic_bitset<> blade;  
 typedef map<blade, long double> clifford;
+typedef std::tuple<blade, int> blade_and_sign;
 
 clifford prepare(const List &L, const NumericVector &d, const NumericVector &m){
     clifford out;
     const unsigned n=L.size();
 
-    for(int i=0; i<n ; i++){
+    for(int i=0 ; i<n ; i++){
         if(d[i] != 0){
             Rcpp::IntegerVector iv = as<Rcpp::IntegerVector> (L[i]);
             blade b;
@@ -34,31 +37,45 @@ clifford prepare(const List &L, const NumericVector &d, const NumericVector &m){
     return(out);
 }
 
-List makeblades(const clifford C){  // takes a clifford object, returns the blades
+Rcpp::IntegerVector which(const blade b){ // takes a blade, returns which(blade)
+    Rcpp::IntegerVector out;
+    unsigned int i;
+    for(i=0 ; i<b.size() ; ++i){
+        if((bool) b[i]){
+            out.push_back(i+1); // the meat; off-by-one here
+        }
+    }
+    return out;
+}
+
+List Rblades(const clifford C){  // takes a clifford object, returns a list of which(blades); used in retval()
     List out;
     clifford::const_iterator ic;
-    
     for(ic=C.begin(); ic != C.end(); ++ic){
-        out.push_back(ic->first);
+        out.push_back(which(ic->first));
     }
     return(out);
 }
 
-NumericVector makecoeffs(const clifford C){  // takes a clifford object, returns the coefficients
+NumericVector coeffs(const clifford C){  // takes a clifford object, returns the coefficients
     NumericVector out(C.size());
     unsigned int i=0;
     clifford::const_iterator ic;   // it iterates through a hyper2 object
     
     for(ic=C.begin(); ic != C.end(); ++ic){
-        out(i++) = ic->second;   // initialize-and-fill is more efficient than  out.push_back(it->second) 
+        const blade b = ic->first;
+        for(i=0 ; i < b.size() ; ++i){
+            if(b[i]==1){
+                out.push_back(i+1);
+            }
+        }
     }
-    return(out);
+    return out;
 }
 
 List retval(const clifford &C){  // used to return a list to R
-    
-        return List::create(Named("blades") =  makeblades(C),
-                            Named("coeffs") =  makecoeffs(C)
+        return List::create(Named("blades") =  Rblades(C),
+                            Named("coeffs") =  coeffs(C)
                             );
 }
 
@@ -91,20 +108,43 @@ List add(
           const List &L2, const NumericVector &c2,  // c[12] = coeffs
           const NumericVector &m
           ){
-
     return retval(c_add(prepare(L1,c1,m),prepare(L2,c2,m)));
 }
 
-
-blade juxtapose(const blade b1, const blade b2){
-    signed int sign;
-    blade::iterator i1,i2;
+blade_and_sign juxtapose(const blade b1, const blade b2, const unsigned int signature){  // takes two blades and returns a reduced version and a sign
+    int sign = 1;
+    blade bout;
+    for(int i=0 ; i<b1.size() ; ++i){
+        if       (((bool)~b1[i]) & ((bool)~b1[i])){ bout[i] = true;   // neither
+        } else if(((bool) b1[i]) & ((bool)~b1[i])){ bout[i] = true;   // just b1
+        } else if(((bool)~b1[i]) & ((bool) b1[i])){ bout[i] = true;   // just b2
+        } else if(((bool) b1[i]) & ((bool) b1[i])){ bout[i] = false;  // both...
+            if(i>signature){sign *= -1;};  // ...swap sign!  NB check for off-by-one error
+        }
+    }
+    return std::make_tuple(bout,sign);
 }
 
-clifford prod(const clifford C1, const clifford C2){
+clifford prod(const clifford C1, const clifford C2, unsigned int signature){
     clifford out;
+    clifford::const_iterator ic1,ic2;
+    blade b;
+    int sign;
+
+    for(ic1=C1.begin(); ic1 != C1.end(); ++ic1){
+        for(ic2=C2.begin(); ic2 != C2.end(); ++ic2){
+            tie(b, sign) = juxtapose(ic1->first, ic2->first, signature);
+            out[b] += sign*(ic2->second)*(ic2->second); // the meat
+        }
+    }
+    return out;
 }
 
+clifford power(const clifford C, unsigned int p){
+
+
+
+}
 
 
 
