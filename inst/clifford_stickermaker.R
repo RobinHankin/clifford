04f4abd73a4f12a2d4576347aad270f9059c2213
Robin Hankin <hankin.robin@gmail.com>
2021-08-06 14:08:40 +1200

    consistentify colour

--- inst/clifford_stickermaker.R
+++ inst/clifford_stickermaker.R
@@ -7,5 +7,5 @@ library("magrittr")
 
 imgurl <- system.file("clifford_blue.png", package="clifford")
 sticker(imgurl, package="clifford", p_size=8, s_x=1, s_y=.95,
-s_width=1, asp=0.85, white_around_sticker=TRUE, h_fill="#5533FF",
+s_width=1, asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="clifford.png")
