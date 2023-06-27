7450b5f83ba4f46d847483dff17b8a72ed02c004
Robin Hankin <hankin.robin@gmail.com>
2023-06-27 15:39:48 +1200

    extra content given by latest version of Rcpp::compileAttributes()

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // c_identity
 List c_identity(const List& L, const NumericVector& p, const NumericVector& m);
 RcppExport SEXP _clifford_c_identity(SEXP LSEXP, SEXP pSEXP, SEXP mSEXP) {
