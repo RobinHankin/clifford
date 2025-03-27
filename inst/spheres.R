bb5e4d74517a1c3d446c50327d39c76561b740e2
RobinHankin <hankin.robin@gmail.com>
2025-03-27 13:22:59 +0000

    slight streamlining

--- inst/spheres.R
+++ inst/spheres.R
@@ -23,18 +23,25 @@ p02 <- sphere(u_c, u_r, c(6,-5,3))
 p03 <- sphere(u_c, u_r, c(0,4,0))
 p04 <- sphere(u_c, u_r, c(1,0,0))
 
+S <- p01 ^ p02 ^ p03 ^ p04 / pseudoscalar()            
+radius <- sqrt(drop((S / drop(S %.% einf))^2))
 
-print(S_star <- p01 ^ p02 ^ p13 ^ p22)  # OPNS
-S <- S_star/pseudoscalar()              # IPNS
 
 S_sand <- S * einf * S                  #sandwich product
 
 scaling_factor <- -zap(S_sand %.% (e(4) + e(5)))
 
 center_cartesian <- getcoeffs(zap(S_sand / scaling_factor), 1:3)
-
-radius <- sqrt(drop((S/drop(S %.% einf))^2))
-
 print(center_cartesian)
 print(radius)
 
+
+"
+Solve[{
+    (x-x2)^2 + (y-y2)^2 + (z-z2)^2 == (x-x1)^2 + (y-y1)^2 + (z-z1)^2,
+    (x-x3)^2 + (y-y3)^2 + (z-z3)^2 == (x-x1)^2 + (y-y1)^2 + (z-z1)^2,
+    (x-x4)^2 + (y-y4)^2 + (z-z4)^2 == (x-x1)^2 + (y-y1)^2 + (z-z1)^2
+},{
+    x,y,z
+}]
+"
