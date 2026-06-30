90aeadb3a47ce0dfb669111a1671117393820820
RobinHankin <hankin.robin@gmail.com>
2026-06-30 09:11:48 +0100

    take unclass() for hashcal consistently

--- R/clifford.R
+++ R/clifford.R
@@ -15,9 +15,17 @@
 }
 
 setOldClass("clifford")
-`terms` <-  function(x){disord(x[[1]], hashcal(x))}  # accessor methods start ...
-`coeffs` <- function(x){disord(x[[2]], hashcal(x))}  # ... continue ...
-`getcoeffs` <- function(C, B){                       # ... accessor methods end
+`terms` <-  function(x){  # accessor methods start ...
+    x <- unclass(x)
+    disord(x[[1]], hashcal(x))
+}  
+
+`coeffs` <- function(x){  # ... continue ...
+    x <- unclass(x)
+    disord(x[[2]], hashcal(x))
+}
+
+`getcoeffs` <- function(C, B){ # ... accessor methods end
     out <- 
     c_getcoeffs(
         L = terms(C),
@@ -161,7 +169,7 @@ setGeneric("dim")
     } else {
         out <- unlist(lapply(terms(x), length))
     } 
-    return(disord(out, hashcal(x)))
+    return(disord(out, hashcal(unclass(x))))
 }
 
 `scalar` <- function(x=1){clifford(list(numeric(0)), x)}
@@ -365,31 +373,31 @@ setMethod("drop","clifford", function(x){drop_clifford(x)})
 }
 
 `gradesplus` <- function(x){
-    if(is.zero(x)){return(disord(numeric(0), hashcal(x)))}
+    if(is.zero(x)){return(disord(numeric(0), hashcal(unclass(x))))}
     p <- signature()[1]
-    return(disord(unlist(lapply(terms(x), function(o){sum(o <= p)})), hashcal(x)))
+    return(disord(unlist(lapply(terms(x), function(o){sum(o <= p)})), hashcal(unclass(x))))
 }
 
 `gradesminus` <- function(x){
-    if(is.zero(x)){return(disord(numeric(0), hashcal(x)))}
+    if(is.zero(x)){return(disord(numeric(0), hashcal(unclass(x))))}
     p <- signature()[1]
     q <- signature()[2]
     return(disord(unlist(lapply(terms(x),
                                 function(o){
                                     sum((o > p) & (o <= p+q))
                                 }
-                                )),hashcal(x)))
+                                )),hashcal(unclass(x))))
 }
 
 `gradeszero` <- function(x){
-    if(is.zero(x)){return(disord(numeric(0), hashcal(x)))}
+    if(is.zero(x)){return(disord(numeric(0), hashcal(unclass(x))))}
     p <- signature()[1]
     q <- signature()[2]
     return(disord(unlist(lapply(terms(x),
                                 function(o){
                                     sum(o > p+q)
                                 }
-                                )), hashcal(x)))
+                                )), hashcal(unclass(x))))
 }
 
 `first_n_last` <- function(x){
